1fb7f70ff2b449ceabb2583d1e67de6c6a1fce3f
SHYAM <shyam666fg@gmail.com>
2026-07-22 20:34:10 +0530

    Time: 2 ms (48.09%) | Memory: 43.3 MB (84.40%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,34 +1,24 @@
 class Solution {
     public String addBinary(String a, String b) {
-
-        String result = "";
-
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        int i = a.length()-1;
+        int j = b.length()-1;
         int carry = 0;
-
-        while (i >= 0 || j >= 0 || carry > 0) {
-
-            int sum = carry;
-
-            // Must be >= 0, otherwise index 0 gets skipped
-            if (i >= 0) {
-                sum = sum + (a.charAt(i) - '0');
+        StringBuffer sb =new StringBuffer();
+        while(i >=0 || j >=0 || carry==1){
+            int sum=carry;
+            if(i>=0){
+                sum+=(a.charAt(i)-'0');
                 i--;
             }
-
-            if (j >= 0) {
-                sum = sum + (b.charAt(j) - '0');
+            if(j>=0){
+                sum+=(b.charAt(j)-'0');
                 j--;
             }
-
-            // Add current answer digit to the FRONT
-            result = (sum % 2) + result;
-
-            // Calculate carry
-            carry = sum / 2;
+            sb.append(sum%2);
+            carry=(sum/2);
         }
+       return sb.reverse().toString();
 
-        return result;
+
     }
 }
